b4c3841dee36e20597400d45a9b88b5c27ea9a00
Elham Bigdeli <elham.bigdeliii@gmail.com>
2024-08-30 22:03:59 +0330

    wheatger-getter; Changes Based On review; 2024-08-30

--- weather_getter/app.py
+++ weather_getter/app.py
@@ -2,22 +2,53 @@ import requests as req
 from datetime import datetime
 from geopy.geocoders import Nominatim
 import json
+import re
 
 
 city_name = input("Please Enter City Name:")
 
+if re.fullmatch(r'^[a-zA-Z\s]+$',city_name) == None:
+    raise Exception("The city name is invalid.")
+
 geo_locator = Nominatim(user_agent="EB_weather_getter")
 location = geo_locator.geocode(city_name)
-latitude = location.latitude
-longitude = location.longitude
 
-url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m'
+if location:
+    if hasattr(location,'latitude'):
+        latitude = location.latitude
+    else:
+        raise Exception("The city doesn't have latitude.")
+    
+    if hasattr(location,'longitude'):
+        longitude = location.longitude
+    else:
+        raise Exception("The city doesn't have longitude.")
+else:
+    raise Exception("The city name isn't valid.")
 
-response = req.get(url)
+
+url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m'
+
+try:
+    response = req.get(url,timeout=1) #1 second
+except req.exceptions.RequestException as e:
+    raise Exception(f"Connection error: {e}")
+
+if response.status_code != 200:
+    raise Exception(f'The connection failed because of: {response.reason}')
+elif response.content == b"":
+    raise Exception("The response is null")
 
 curent_date = datetime.today().strftime('%Y-%m-%d %H-%M-%S')
 file_name = city_name + '_' + curent_date + '.txt'
-file = open(file_name,"w")
-file.write(json.dumps(response.json(),indent=4))
-file.close()
 
+with open(file_name,"w") as resultFile:
+    resultFile.write(json.dumps(response.json(),indent=4))
+
+# if response.json().get('current') is not None:
+# if 'current' in response.json()
+try:
+    current_temp = response.json()['current']['temperature_2m']
+    print(f"{city_name}'s current temp is: {current_temp}")
+except KeyError:
+    raise Exception(f"An Error occur during get response. Please try later. Error is KeyError")
